a3a3f67ffbd3afdfec8c8284d0779b5d4f2c0aa5
AkashDhiman23 <akash.dhiman5555@gmail.com>
2025-06-25 17:09:02 +1200

    updated requirement.txt file

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -164,8 +164,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'akashdhiman181999@gmail.com'
-EMAIL_HOST_PASSWORD = 'dgyy yoem kfrc iuvl'
+EMAIL_HOST_USER = 'organizemyyspace@gmail.com'
+EMAIL_HOST_PASSWORD = 'qdjb eieu szck sxbj'
 
 
 AUTHENTICATION_BACKENDS = ['accounts.backends.EmailBackend']
